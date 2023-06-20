9dd438ab8dde52c5e07412d0cc3388ae2efcd813
Dopeul <n.bouniqmercier@gmail.com>
2023-06-20 10:52:50 +0200

    creation navbar et aside

--- TpCalendar/src/app/app.module.ts
+++ TpCalendar/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DailyComponent } from './Component/daily/daily.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+import { SidebarComponent } from './Component/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DailyComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
