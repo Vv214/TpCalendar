8690966e974a3fc94527119a07bb8e953b9e603d
smaile <smail_lakermi@yahoo.fr>
2023-06-22 14:18:58 +0200

    ajout class admin

--- src/app/Service/Client.ts
+++ src/app/Service/Client.ts
@@ -1,14 +1,14 @@
-export class Client {
-    prenom: string;
-    nom: string;
-    message: string;
-    telephone: string;
-    email: string;
-    constructor(prenom: string, nom: string, message: string, telephone: string, email: string) {
-        this.prenom = prenom;
-        this.nom = nom;
-        this.message = message;
-        this.telephone = telephone;
-        this.email = email;
-    }
-}
+// export class Client {
+//     prenom: string;
+//     nom: string;
+//     message: string;
+//     telephone: string;
+//     email: string;
+//     constructor(prenom: string, nom: string, message: string, telephone: string, email: string) {
+//         this.prenom = prenom;
+//         this.nom = nom;
+//         this.message = message;
+//         this.telephone = telephone;
+//         this.email = email;
+//     }
+// }
--- src/model/Creneau.ts
+++ src/model/Creneau.ts
@@ -1,17 +1,12 @@
 export class Creneau {
-// date : Date ;
-annee: number;
-mois: number;
-jour:number ;
-heure: number;
-// constructor(date:Date){
-//  this.date = date;
-// }
-constructor(annee: number,mois: number, jour:number , heure: number){
-    this.annee =annee;
-    this.mois = mois;
-    this.jour = jour;
-    this.heure = heure;
+ date : Date ;
+ id :number = 0;
+
+constructor(date:Date){
+ this.date = date;
+}
+setId(id:number){
+    this.id= id;
 }
 
 }
